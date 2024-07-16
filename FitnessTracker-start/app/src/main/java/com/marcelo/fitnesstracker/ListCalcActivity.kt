73cf325455e90fc3a422b7ff4a2f07993276071b
Tchelo <tchelospy@gmail.com>
2024-07-15 22:07:46 -0300

    Regra de negocio negócio para listar os registros com RecyclerView

--- FitnessTracker-start/app/src/main/java/com/marcelo/fitnesstracker/ListCalcActivity.kt
+++ FitnessTracker-start/app/src/main/java/com/marcelo/fitnesstracker/ListCalcActivity.kt
@@ -1,15 +1,41 @@
 package com.marcelo.fitnesstracker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.marcelo.fitnesstracker.Adapters.ListCalcAdapter
+import com.marcelo.fitnesstracker.model.Calc
 
 class ListCalcActivity : AppCompatActivity() {
+    private lateinit var rvListCalc: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_calc)
 
+        val type =
+            intent?.extras?.getString("type") ?: throw IllegalStateException("Type not found")
+
+        Thread {
+            val app = application as App
+            val calcDao = app.db.calcDao()
+            val registerByType = calcDao.getRegisterByType(type)
+
+            runOnUiThread {
+                Log.i("Type", "registerByType: $registerByType\n")
+
+                val listItem = mutableListOf<Calc>()
+                for (item in registerByType) {
+                    listItem.add(item)
+                }
+
+                rvListCalc = findViewById(R.id.rv_list_calc)
+                val adapter = ListCalcAdapter(listItem)
+                rvListCalc.adapter = adapter
+                rvListCalc.layoutManager = LinearLayoutManager(this)
+            }
+        }.start()
     }
 }
