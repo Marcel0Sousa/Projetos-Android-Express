6baf1064ce3354e97b52925982dc98d855f5b452
Tchelo <tchelospy@gmail.com>
2024-07-16 15:05:02 -0300

    Formatando o resultado da lista

--- FitnessTracker-start/app/src/main/java/com/marcelo/fitnesstracker/Adapters/ListCalcAdapter.kt
+++ FitnessTracker-start/app/src/main/java/com/marcelo/fitnesstracker/Adapters/ListCalcAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.marcelo.fitnesstracker.R
 import com.marcelo.fitnesstracker.model.Calc
+import java.text.SimpleDateFormat
 import java.util.Locale
 
 class ListCalcAdapter(
@@ -15,7 +16,8 @@ class ListCalcAdapter(
     RecyclerView.Adapter<ListCalcAdapter.ListCalcViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListCalcViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_calc_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_calc_item, parent, false)
         return ListCalcViewHolder(view)
     }
 
@@ -29,12 +31,17 @@ class ListCalcAdapter(
     }
 
     inner class ListCalcViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(listCorrent: Calc) {
-            val result: TextView = itemView.findViewById(R.id.response_item)
-            val type: TextView = itemView.findViewById(R.id.tv_type)
+        fun bind(item: Calc) {
+            val type: TextView = itemView.findViewById(R.id.tv_type_item)
+            val result: TextView = itemView.findViewById(R.id.tv_response_item)
+            val date: TextView = itemView.findViewById(R.id.tv_date_item)
 
-            type.text = listCorrent.type.uppercase(Locale.getDefault())
-            result.text = listCorrent.response.toString()
+            val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale("pt", "BR"))
+            date.text = simpleDateFormat.format(item.createdDate)
+
+            type.text = item.type.uppercase(Locale.getDefault())
+            result.text = item.response.toString().substring(0, 5)
+
         }
 
     }
