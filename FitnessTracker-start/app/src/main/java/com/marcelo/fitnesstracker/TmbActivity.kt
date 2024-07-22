8b175889207acd89aaa0d679990338e4e5e1bafd
Tchelo <tchelospy@gmail.com>
2024-07-22 16:15:00 -0300

    Add caixa de texto de auto-seleção

--- FitnessTracker-start/app/src/main/java/com/marcelo/fitnesstracker/TmbActivity.kt
+++ FitnessTracker-start/app/src/main/java/com/marcelo/fitnesstracker/TmbActivity.kt
@@ -1,14 +1,33 @@
 package com.marcelo.fitnesstracker
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 class TmbActivity : AppCompatActivity() {
+
+    private lateinit var lifestyle: AutoCompleteTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tmb)
+
+        val actionBar: ActionBar
+        actionBar = supportActionBar!!
+        val colorDrawable = ColorDrawable(getColor(R.color.color_tmb))
+        actionBar.setBackgroundDrawable(colorDrawable)
+
+
+        lifestyle = findViewById(R.id.auto_lifestyle)
+        val items = resources.getStringArray(R.array.tmb_lifestyle)
+        lifestyle.setText(items.first())
+        val adapter = ArrayAdapter(this@TmbActivity, android.R.layout.simple_list_item_1, items)
+        lifestyle.setAdapter(adapter)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -17,7 +36,7 @@ class TmbActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.menu_search -> {
                 finish()
             }
