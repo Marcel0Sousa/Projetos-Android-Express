ce92655699ad2159a70d41745ffd8f9cba04a5db
Marcel0Sousa <tchelospt@gmail.com>
2024-10-20 21:17:55 -0300

    Manipulação de desenhaveis - sombreamento

--- NetflixRemake/app/src/main/java/com/marcelo/sousa/netflixremake/MovieActivity.kt
+++ NetflixRemake/app/src/main/java/com/marcelo/sousa/netflixremake/MovieActivity.kt
@@ -1,8 +1,11 @@
 package com.marcelo.sousa.netflixremake
 
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,18 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // busca o desenhavel (layer-list)
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+
+        // busca a capa do filme
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+
+        // atribui a nova capa ao layer-list
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+
+        // set a capa no ImageView
+        val imageCover: ImageView = findViewById(R.id.movie_img)
+        imageCover.setImageDrawable(layerDrawable)
     }
 }
